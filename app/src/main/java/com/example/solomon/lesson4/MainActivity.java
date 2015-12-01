151471b9dd85b02c4b8d116ccb3a276667df12e6
Solomon <alwayssm@gmail.com>
2015-12-01 07:07:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/solomon/lesson4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.solomon.lesson4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
